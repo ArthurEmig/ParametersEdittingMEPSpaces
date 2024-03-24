4aacfde6161e8df636c6523dd6e7889caa5fb2aa
ArthurEmig <arthur.emig@burohappold.com>
2024-03-24 12:54:43 +0100

    Combo Box for Family Names populated

--- ParamTransfer.extension/Param_Transfer_Test.tab/ParamTransfer.panel/ParamTransfer.pushbutton/script.py
+++ ParamTransfer.extension/Param_Transfer_Test.tab/ParamTransfer.panel/ParamTransfer.pushbutton/script.py
@@ -1,4 +1,7 @@
 from pyrevit import script, forms
+from pyrevit import revit, DB, UI
+from pyrevit import HOST_APP, EXEC_PARAMS
+import Autodesk.Revit.DB as db
 import qa_engine
 import filemgr
 from qa_report import print_reports
@@ -6,7 +9,9 @@ import clr
 clr.AddReference('System.Windows.Forms')
 clr.AddReference('IronPython.wpf')
 
-xamlfile = script.get_bundle_file('SelectFamilyTypesParams.xaml')
+from System.Windows.Controls import ComboBox, ComboBoxItem  # Import ComboBoxItem
+
+xamlfile = script.get_bundle_file('SelectMEPFamilyType.xaml')
 
 import wpf
 from System import Windows
@@ -16,6 +21,59 @@ class MyWindow(Windows.Window):
     def __init__(self):
         wpf.LoadComponent(self, xamlfile)
 
+    def btn_ok_clicked():
+        pass
 
-MyWindow().ShowDialog()
+    def populate_combo_box_family_types(self, doc):
+        all_MEP_family_types = filtered_collector.OfClass(db.FamilySymbol).ToElements()
+
+        quantity_MEP_Elements = len(all_MEP_family_types)
+
+        all_family_names = list(set([elem.Family.Name for elem in all_MEP_family_types]))
+
+        for family_name in all_family_names:
+            combobox_item = ComboBoxItem()
+            combobox_item.Content = family_name
+            self.combo_family_type.Items.Add(combobox_item)
+        
+    def comboBoxFamilyTypes_SelectionChanged(self, sender, e):
+        selected_item = self.combo_family_type.SelectedItem
+        if selected_item:
+            selected_value = selected_item.Content
+            print(selected_value)
+        else:
+            print("No Item Selected")
+        
+
+
+
+# MyWindow().ShowDialog()
+
+if __name__ == "__main__":
+    doc = revit.doc
+    uidoc = HOST_APP.uidoc
+    filtered_collector = DB.FilteredElementCollector(doc)
+
+    # all_MEPFamilyTypes = DB.FilteredElementCollector(doc).OfCategory(db.BuiltInCategory.OST_DuctAccessory).ToElements()
+
+    # all_MEP_family_types = filtered_collector.OfClass(db.FamilySymbol).ToElements()
+
+    # quantity_MEP_Elements = len(all_MEP_family_types)
+
+    # all_family_names = list(set([elem.Family.Name for elem in all_MEP_family_types]))
+
+    # for family_name in all_family_names:
+    #     combobox_item = ComboBoxItem()
+    #     combobox_item.Content = family_name
+    #     self.combo_family_type.Items.Add(combobox_item)
+
+    # all_family_types = list(set([elem.GetTypeId() for elem in all_MEP_family_types]))
+
+    my_window_obj = MyWindow()
+    my_window_obj.populate_combo_box_family_types(doc)
+    my_window_obj.ShowDialog()
+
+    # MyWindow().ShowDialog()
+
+    # print("all MEP: ", all_family_types)
 
