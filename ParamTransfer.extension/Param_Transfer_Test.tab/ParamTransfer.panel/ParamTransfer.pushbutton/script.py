d70e3919de92a5e5a6ff7e23e45a1254604220f8
ArthurEmig <arthur.emig@burohappold.com>
2024-03-24 15:42:29 +0100

    collect MEP Families Instancies Parameters and populate the corresponding Combo Boxes

--- ParamTransfer.extension/Param_Transfer_Test.tab/ParamTransfer.panel/ParamTransfer.pushbutton/script.py
+++ ParamTransfer.extension/Param_Transfer_Test.tab/ParamTransfer.panel/ParamTransfer.pushbutton/script.py
@@ -9,21 +9,31 @@ import clr
 clr.AddReference('System.Windows.Forms')
 clr.AddReference('IronPython.wpf')
 
+
+
 from System.Windows.Controls import ComboBox, ComboBoxItem  # Import ComboBoxItem
 
-xamlfile = script.get_bundle_file('SelectMEPFamilyType.xaml')
+xamlfile_family_selection = script.get_bundle_file('SelectMEPFamilyType.xaml')
+
+xamlfile_parameter_pairs_selection = script.get_bundle_file('SelectParams.xaml')
 
 import wpf
 from System import Windows
 
-class MyWindow(Windows.Window):
+
+family_instance_parameters = []
+family_instance_parameters_names = []
+params_names_vs_params_objs_dict = {}
+
+class FamilySelectionWindow(Windows.Window):
 
     def __init__(self):
-        wpf.LoadComponent(self, xamlfile)
+        wpf.LoadComponent(self, xamlfile_family_selection)
         self.selected_value = None
 
-    def btn_ok_clicked():
-        pass
+    def btn_ok_clicked(self, sender, e):
+        self.Close()
+        ParameterPairsSelectionWindow(self.selected_value).ShowDialog()
 
     def populate_combo_box_family_types(self, doc):
         all_MEP_family_types = filtered_collector.OfClass(db.FamilySymbol).ToElements()
@@ -44,6 +54,116 @@ class MyWindow(Windows.Window):
             print(self.selected_value)
         else:
             print("No Item Selected")
+        collector = DB.FilteredElementCollector(doc).OfClass(db.FamilySymbol).ToElements()
+        selected_family_symbol = next((fs for fs in collector if fs.Family.Name == self.selected_value), None)
+
+        if selected_family_symbol:
+            # Get all parameters of the family symbol
+            parameters = selected_family_symbol.Parameters
+            print("Parameters:")
+            for parameter in parameters:
+                print(parameter.Definition.Name)
+
+
+
+class ParameterPairsSelectionWindow(Windows.Window):
+
+    def __init__(self, selected_value):
+        wpf.LoadComponent(self, xamlfile_parameter_pairs_selection)
+
+        self.selected_value = selected_value
+
+        self.selected_value_space_1 = None
+        self.selected_value_MEP_Instance_1 = None
+
+        self.selected_value_space_2 = None
+        self.selected_value_MEP_Instance_2 = None
+
+        self.selected_value_space_3 = None
+        self.selected_value_MEP_Instance_3 = None
+
+        self.selected_value_space_4 = None
+        self.selected_value_MEP_Instance_4 = None
+        # Find the Family object corresponding to the "MyFamily" family
+        family_name = self.selected_value # Replace "MyFamily" with the actual name of your family
+        family_collector = DB.FilteredElementCollector(doc).OfClass(DB.Family)
+        my_families = [f for f in family_collector if f.Name == family_name]
+
+        
+
+        if my_families:
+            for my_family in my_families:
+            # Get the ElementId of the "MyFamily" family
+                family_ids = list(my_family.GetFamilySymbolIds())
+
+                print(type(family_ids))
+                print(family_ids)
+
+                elem_symbol = doc.GetElement(family_ids[0])
+
+                print("Symbol: ", type(elem_symbol))
+                family_instances_of_selected_family = []
+
+                # Define the filter for FamilyInstances of the "MyFamily" family
+                for family_id in family_ids:
+                    family_instance_filter = DB.FamilyInstanceFilter(doc, family_id)
+
+                    # Get all family instances of the "MyFamily" family
+                    family_instance_collector = DB.FilteredElementCollector(doc).WherePasses(family_instance_filter)
+                    my_family_instances = family_instance_collector.ToElements()
+
+                    family_instances_of_selected_family += my_family_instances
+
+                    for family_instance in my_family_instances:
+                        for param in family_instance.Parameters:
+                            if param not in family_instance_parameters:
+                                param_name = param.Definition.Name
+
+
+                                # Populate param names dictionary, to find each param on its Definition.Name
+                                if param_name not in params_names_vs_params_objs_dict:
+                                    params_names_vs_params_objs_dict[param_name] = [param]
+                                else:
+                                    temp_list_params = params_names_vs_params_objs_dict[param_name]
+                                    temp_list_params.append(param)
+                                family_instance_parameters.append(param)
+                                params_names_vs_params_objs_dict[param_name] = temp_list_params
+
+                                # Populate Combo Boxes
+                                if param_name not in family_instance_parameters_names:
+                                    family_instance_parameters_names.append(param_name)
+                                    combobox_item_1 = ComboBoxItem()
+                                    combobox_item_2 = ComboBoxItem()
+                                    combobox_item_3 = ComboBoxItem()
+                                    combobox_item_4 = ComboBoxItem()
+                                    combobox_item_1.Content = param_name
+                                    combobox_item_2.Content = param_name
+                                    combobox_item_3.Content = param_name
+                                    combobox_item_4.Content = param_name
+                                    self.combo_param_to_1.Items.Add(combobox_item_1)
+                                    self.combo_param_to_2.Items.Add(combobox_item_2)
+                                    self.combo_param_to_3.Items.Add(combobox_item_3)
+                                    self.combo_param_to_4.Items.Add(combobox_item_4)
+
+                    # print("So many Instances of {}: {} ".format(family_id, len(my_family_instances)))
+
+                # Now you can process the family instances as needed
+                for my_family_instance in my_family_instances:
+                    # Do something with each family instance
+                    pass
+        else:
+            print("Family 'MyFamily' not found.")
+
+        
+
+    def btn_ok_clicked(self, sender, e):
+        
+        
+        self.Close()
+
+    
+    
+
         
 
 
@@ -70,7 +190,7 @@ if __name__ == "__main__":
 
     # all_family_types = list(set([elem.GetTypeId() for elem in all_MEP_family_types]))
 
-    my_window_obj = MyWindow()
+    my_window_obj = FamilySelectionWindow()
     my_window_obj.populate_combo_box_family_types(doc)
     my_window_obj.ShowDialog()
 
